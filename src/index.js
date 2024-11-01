b13dc0dc259ae4e3fe975454a84a65b6c912a41d
SakuraFubukii <woozie.wzt@gmail.com>
2024-11-01 21:49:40 +0800

    UI Prototype

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import session from 'express-session';
+import path from 'path';
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(
+  session({
+    secret: 'polyu-park',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { httpOnly: true },
+  })
+);
+
+app.use(express.static('static'));
+
+app.get('/booking', (req, res) => {
+  res.sendFile(path.join(__dirname, 'booking.html'));
+});
+
+app.get('/payment', (req, res) => {
+  res.sendFile(path.join(__dirname, 'payment.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started at http://127.0.0.1:${PORT}`);
+});
